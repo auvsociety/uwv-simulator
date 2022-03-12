719caa5db12c4265edc25ef01a77f25dcc5209e4
Rahulkannan S <rahulknns@gmail.com>
2022-03-12 07:44:55 +0530

    completed hardware_controller.cpp

--- uwv_control/src/vec6/common/vec6_hardwarecontroller.cpp
+++ uwv_control/src/vec6/common/vec6_hardwarecontroller.cpp
@@ -39,11 +39,30 @@ void Vec6HardwareController::initController(ros::NodeHandle& _node, double _max_
 }
 
 void Vec6HardwareController::heavePid2Effort(double _pid_heave){
-	ROS_ERROR_STREAM("vec6's HardwareController NOT FULLY DEFINED.");
+	if (!state_.is_traversing_){
+		ROS_INFO_STREAM("vec6 is not in the traversing mode.");
+		return;
+	}
+    effort_.effort[M_PORT] = _pid_heave;
+    effort_.effort[M_STAR] = _pid_heave;
 }
 
 void Vec6HardwareController::vectoredPid2Effort(double _pid_surge, double _pid_yaw, double _pid_sway){
-	ROS_ERROR_STREAM("vec6's HardwareController NOT FULLY DEFINED.");
+	if (!state_.is_traversing_){
+		ROS_INFO_STREAM("vec6 is not in the traversing mode.");
+		return;
+  	}
+
+	effort_.effort[F_PORT] = ((1-state_.beta_)/2)*state_.sys_mat_[0][0]*_pid_surge + state_.beta_*state_.sys_mat_[1][0]*_pid_yaw + ((1-state_.beta_)/2)*state_.sys_mat_[2][0]*_pid_sway;
+	effort_.effort[F_STAR] = ((1-state_.beta_)/2)*state_.sys_mat_[0][1]*_pid_surge + state_.beta_*state_.sys_mat_[1][1]*_pid_yaw + ((1-state_.beta_)/2)*state_.sys_mat_[2][1]*_pid_sway;
+	effort_.effort[B_PORT] = ((1-state_.beta_)/2)*state_.sys_mat_[0][2]*_pid_surge + state_.beta_*state_.sys_mat_[1][2]*_pid_yaw + ((1-state_.beta_)/2)*state_.sys_mat_[2][2]*_pid_sway;
+	effort_.effort[B_STAR] = ((1-state_.beta_)/2)*state_.sys_mat_[0][3]*_pid_surge + state_.beta_*state_.sys_mat_[1][3]*_pid_yaw + ((1-state_.beta_)/2)*state_.sys_mat_[2][3]*_pid_sway;
+		
+	// limiting thrust
+	if(effort_.effort[F_PORT] > state_.max_thrust_) effort_.effort[F_PORT] = state_.max_thrust_ - 8; 
+	if(effort_.effort[F_STAR] > state_.max_thrust_) effort_.effort[F_STAR] = state_.max_thrust_ - 8;
+	if(effort_.effort[B_PORT] > state_.max_thrust_) effort_.effort[B_PORT] = state_.max_thrust_ - 8;
+	if(effort_.effort[B_STAR] > state_.max_thrust_) effort_.effort[B_STAR] = state_.max_thrust_ - 8;
 }
 
 void Vec6HardwareController::effort2PWM(){
@@ -53,15 +72,29 @@ void Vec6HardwareController::effort2PWM(){
 }
 void Vec6HardwareController::sendCommands(void){
    effort2PWM();
-   pwm_pub_.publish(pwm_);
+   Vec6comms.pwm_pub_.publish(pwm_);
 }
 
 void Vec6HardwareController::allThrustersStop(void)
 {
-  ROS_ERROR_STREAM("vec6's HardwareController NOT FULLY DEFINED.");
+  for (int i = 0; i < THRUSTER_NUM; i++)
+  {
+    effort_.effort[i] = 0;
+  }
+  sendCommands();
 }
 
 void Vec6HardwareController::setEffort(double *_manual_effort)
 {
-  ROS_ERROR_STREAM("vec6's HardwareController NOT FULLY DEFINED.");
+  if (!state_.is_traversing_)
+  {
+    ROS_INFO_STREAM("vec6 is not in the traversing mode.");
+    return;
+  }
+
+  for (int i = 0; i < THRUSTER_NUM; i++)
+  {
+    effort_.effort[i] = *(_manual_effort + i);
+  }
+  sendCommands();
 }
